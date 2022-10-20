028583ecc50cf57dd640f37a676f26e29384de17
LucasLLopes02 <116230364+LucasLLopes02@users.noreply.github.com>
2022-10-20 01:17:23 +0000

    Ultimas alteraçoes

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ idade = 20
 
 #Exibir o nome (que esta dentro da variavel nome)
 print(nome)
-#Exibir a idade (qu esta dentro da variavel idade)
-print(idade)
+#Maneiras para quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável "idade"
+print("A minha idade é "+str(idade))
 
+print(f"A minha idade é {idade}")
 
+print("A minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas variáveis "nome" e "idade"
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
 
 '''
 comentario em bloco
