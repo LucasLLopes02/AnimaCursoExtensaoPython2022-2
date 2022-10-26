d798668a98d5360842fad1fbe597952d195f766f
LucasLLopes02 <116230364+LucasLLopes02@users.noreply.github.com>
2022-10-26 23:13:08 +0000

    alterações

---
+++ aula01.py
@@ -0,0 +1,26 @@
+# Meu primeiro projeto Python!!!
+#
+# print() = comando de saída
+
+print("Hello, World!!!!")
+
+#Quando quiser guardar uma String (Frase) 
+nome = "Lucas Lima Lopes"
+#Quando quiser guardar um número inteiro
+idade = 20
+
+#Exibir o nome (que esta dentro da variavel nome)
+print(nome)
+#Maneiras para quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável "idade"
+print("A minha idade é "+str(idade))
+
+print(f"A minha idade é {idade}")
+
+print("A minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas variáveis "nome" e "idade"
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+'''
+comentario em bloco
+'''
--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-# Meu primeiro projeto Python!!!
-#
-# print() = comando de saída
+# 1 comando de entrada input(): quero que o usuario digite algo
+nome = input("Digite seu nome: ") 
+#peça a idade para o usuário
+idade = int(input("Qual sua idade? "))
 
-print("Hello, World!!!!")
+#comando de saída.. exibir na tela
+print("Boa Noite {}".format(nome))
+#exiba sua idade é...
+print("Sua idade é {} anos".format(idade))
 
-#Quando quiser guardar uma String (Frase) 
-nome = "Lucas Lima Lopes"
-#Quando quiser guardar um número inteiro
-idade = 20
-
-#Exibir o nome (que esta dentro da variavel nome)
-print(nome)
-#Maneiras para quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável "idade"
-print("A minha idade é "+str(idade))
-
-print(f"A minha idade é {idade}")
-
-print("A minha idade é {}".format(idade))
-
-#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas variáveis "nome" e "idade"
-print("Meu nome é {} e tenho {} anos".format(nome,idade))
-
-'''
-comentario em bloco
-'''
+#e se eu quiser mostrar o dobro da idade informada..
+dobro = idade * 2
+print("O dobro de sua idade é {}".format(dobro))
