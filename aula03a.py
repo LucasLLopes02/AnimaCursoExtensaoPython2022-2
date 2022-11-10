89ac7527d25e132d2f05cd2027f5236da5c259a6
LucasLLopes02 <116230364+LucasLLopes02@users.noreply.github.com>
2022-11-10 00:02:58 +0000

    ok

---
+++ aula03a.py
@@ -0,0 +1,42 @@
+#exibir numeros de 1 a 10 repetidamente
+contador  = 1
+
+while(contador <= 10):
+  print(contador)
+  contador = contador + 1
+
+#Laço for
+fruta = "morango"
+print(fruta)
+
+#Lista
+frutas = ("morango", "laranja", "pêra")
+
+#exibir todas 
+
+print(frutas)
+
+#exibir apenas a terceira fruta
+
+print(frutas[2])
+
+#exibir quantas frutas tem na minha lista
+print(len(frutas))  #length = tamanho
+
+#incluir uma fruta nova
+#frutas.append("uva")
+#print(len(frutas))  #length = tamanho
+
+print("")
+i=0
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
+
+#exemplo de frutas com for
+print("")
+for fruta in frutas:
+  print(fruta)
+
+
+
--- main.py
+++
@@ -1,5 +0,0 @@
-#exemplo de laço
-#se eu quiser exibir números de 1 a 10
-numero = 1
-print(numero)
-numero = numero + 1
